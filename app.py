972133ed57de3498ee7d40e943ecb4f53506ef3c
John Mack <johnmack@Johns-iMac-2.local>
2020-02-11 15:49:36 -0700

    initial route created and jinjda templated

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template
 from boggle import Boggle
 
+app = Flask(__name__)
+
+
 boggle_game = Boggle()
+
+
+@app.route('/')
+def main():
+    board = boggle_game.make_board()
+    return render_template("game.html", board=board)
