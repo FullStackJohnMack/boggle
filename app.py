4b6d6f3dbf2aa6f7054d68cfb4cf2fb925058cea
John <john@anon.local>
2020-02-13 15:10:36 -0700

    needs finalizing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request, redirect
+from flask import Flask, render_template, session, request, redirect, flash
 from boggle import Boggle
 
 
@@ -6,18 +6,62 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "password"
 
 boggle_game = Boggle()
+entered_words = []
 
 
 @app.route('/')
 def main():
+    """Initializes the game and renders the game board HTML"""
     board = boggle_game.make_board()
     session['board'] = board
+    session['completed_games'] = 0
     return render_template("game.html", board=board)
 
 
 @app.route('/check-word')
 def check_word():
+    """Checks if a word is in the dictionary and on the board or if that word has already been used"""
     word = request.args["word"]
     board = session['board']
     result = {"result": f"{boggle_game.check_valid_word(board, word)}"}
+    if word in entered_words:
+        result = {"result": "already-in-list"}
+    else:
+        entered_words.append(word)
     return result
+
+
+@app.route('/end-game')
+def end_game():
+    """Handles end of game actions and returns an object with the current number of completed games"""
+    is_this_high_score = request.args["is_this_high_score"]
+    session["is_this_high_score"] = is_this_high_score
+    game_count = incrementGameCount()
+    result = {
+        "game_count": game_count
+    }
+    return result
+
+
+@app.route('/check-high-score')
+def check_high_score():
+    """Updates high score in session between session high and this game's high and returns the current session high score"""
+    if session.get("high_score") == None:
+        session["high_score"] = session.get("is_this_high_score")
+        high_score = session.get("is_this_high_score")
+    else:
+        if session.get("is_this_high_score") > session.get("high_score"):
+            session["high_score"] = session.get("is_this_high_score")
+    return session.get("high_score")
+
+
+def incrementGameCount():
+    """Updates game count in session after each game and returns the game count"""
+    if session.get("game_count"):
+        session["game_count"] = session["game_count"] + 1
+    else:
+        session["game_count"] = 1
+    return session.get("game_count")
+
+
+# check all docstrings, write tests for all Python views, enhance timer, refactor
--- static/app.js
+++ static/app.js
@@ -1,22 +1,29 @@
 const $button = $("#button");
 const $response = $("#response");
 const $score = $("#high_score");
-let score = 0;
-let timeout = 0;
+let score = 0;  //this score for this game
+let num1StopsGame = 0; //when incremented, words can no longer be submitted by clicking the button
 
+//Renders initial score on game board
 $score.html(`Your score is ${score}`)
 
 
-
+/**
+ * Handles button clicks
+ */
 $button.on('click', (event) => {
     event.preventDefault()
     $response.html("")
     const guess = $("#guess").val()
-    if (timeout != 1)
+    if (num1StopsGame != 1)
         checkIfValidFoundWord(guess);
-
+    $("#guess").val("")
 })
 
+/**
+ * Passes entered word to server to check validity and renders HTML response
+ * Also disables button click submission after specficied timeout
+ */
 async function checkIfValidFoundWord(guess) {
     const resp = await axios.get("/check-word", { params: { word: guess } });
     $response.append(resp.data.result)
@@ -24,7 +31,29 @@ async function checkIfValidFoundWord(guess) {
         score++;
         $score.html(`Your score is ${score}`);
     }
-    setTimeout(function () { timeout = 1 }, 60000);
+    setTimeout(function () {
+        num1StopsGame = 1;
+        updateGameEndStats();
+    }, 15000);
 }
 
-//left off step six
+/**
+ * Updates high score on server side
+ *
+ */
+async function updateGameEndStats() {
+    const resp = await axios.get("/end-game", {
+        params: {
+            is_this_high_score: score
+        }
+    });
+    checkForHighScore()
+}
+
+/**
+ * Updates the high score on the server side
+ *  
+ */
+async function checkForHighScore(potentialHighScore) {
+    const resp = await axios.get("/check-high-score");
+}
